d7f302cbe6f40585899a6dc207efd3ce99b94ae5
hamza <hamza786504@gmail.com>
2023-02-17 06:04:02 +0500

    first delivery

--- js/script.js
+++ js/script.js
@@ -3,19 +3,20 @@
 
 
 
-document.getElementById("burger_btn").addEventListener("click", function(){
+document.getElementById("burger_btn").addEventListener("click", function () {
     document.querySelector(".navigation").style.display = "block";
     document.getElementById("burger_btn").style.display = "none";
     document.getElementById("close_btn").style.display = "block";
 });
 
-document.getElementById("close_btn").addEventListener("click", function(){
+document.getElementById("close_btn").addEventListener("click", function () {
     document.querySelector(".navigation").style.display = "none";
     document.getElementById("burger_btn").style.display = "flex";
     document.getElementById("close_btn").style.display = "none";
 });
 
-document.getElementById("moon_btn").addEventListener("click" , function(){
+document.getElementById("moon_btn").addEventListener("click", function () {
+    document.documentElement.style.setProperty('--body-background', '#191627');
     document.documentElement.style.setProperty('--body-background', '#191627');
     document.getElementById("footer").style.backgroundColor = "#17161f";
     const tag = document.querySelectorAll(".service_card");
@@ -27,17 +28,17 @@ document.getElementById("moon_btn").addEventListener("click" , function(){
     document.documentElement.style.setProperty('--text-color', '#fbfbfe');
     const elements = document.querySelectorAll("hero_section icon_box a");
     Array.from(elements).forEach(element => {
-      element.style.color = "white";
+        element.style.color = "white";
     });
 
 
 
-    if(window.innerWidth > 768){
+    if (window.innerWidth > 768) {
         document.getElementById("burger_btn").style.display = "none";
         document.getElementById("close_btn").style.display = "none";
         document.getElementById("moon_btn").style.display = "none";
         document.getElementById("sun_btn").style.display = "block";
-    }else{
+    } else {
         document.querySelector(".navigation").style.display = "none";
         document.getElementById("burger_btn").style.display = "flex";
         document.getElementById("close_btn").style.display = "none";
@@ -47,7 +48,7 @@ document.getElementById("moon_btn").addEventListener("click" , function(){
 });
 
 
-document.getElementById("sun_btn").addEventListener("click" , function(){
+document.getElementById("sun_btn").addEventListener("click", function () {
     document.documentElement.style.setProperty('--body-background', '#fbfbfe');
     document.documentElement.style.setProperty('--text-color', '#191627');
     const tag = document.querySelectorAll(".service_card");
@@ -58,10 +59,10 @@ document.getElementById("sun_btn").addEventListener("click" , function(){
 
     document.getElementById("footer").style.backgroundColor = "#6e57e0";
     document.getElementById("moon_btn").style.display = "block";
-        document.getElementById("sun_btn").style.display = "none";
-    if(window.innerWidth > 768){
-        
-    }else{
+    document.getElementById("sun_btn").style.display = "none";
+    if (window.innerWidth > 768) {
+
+    } else {
         document.querySelector(".navigation").style.display = "none";
         document.getElementById("burger_btn").style.display = "flex";
         document.getElementById("close_btn").style.display = "none";
@@ -72,10 +73,10 @@ document.getElementById("sun_btn").addEventListener("click" , function(){
 
 
 
-window.onscroll = function() {
-    if(window.innerWidth < 768){
+window.onscroll = function () {
+    if (window.innerWidth < 768) {
         const display_value = document.querySelector(".navigation").style.display;
-        if(display_value === "block"){
+        if (display_value === "block") {
             document.querySelector(".navigation").style.display = "none";
             document.getElementById("burger_btn").style.display = "flex";
             document.getElementById("close_btn").style.display = "none";
@@ -88,14 +89,14 @@ const educationBtn = document.getElementById("education_btn");
 const workBtn = document.getElementById("work_btn");
 
 workBtn.addEventListener("click", () => {
-  educationBtn.classList.toggle("active");
-  workBtn.classList.toggle("active");
-  document.querySelector(".work_boxes").style.display = "block";
-  document.querySelector(".education_boxes").style.display = "none";
+    educationBtn.classList.toggle("active");
+    workBtn.classList.toggle("active");
+    document.querySelector(".work_boxes").style.display = "block";
+    document.querySelector(".education_boxes").style.display = "none";
 });
 educationBtn.addEventListener("click", () => {
     educationBtn.classList.toggle("active");
     workBtn.classList.toggle("active");
     document.querySelector(".work_boxes").style.display = "none";
-  document.querySelector(".education_boxes").style.display = "block";
-  });
+    document.querySelector(".education_boxes").style.display = "block";
+});
